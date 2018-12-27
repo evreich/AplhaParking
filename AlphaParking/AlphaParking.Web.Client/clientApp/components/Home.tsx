4a7f2dedfc3eba3550f69305d87e4a757417ef49
Vlasov Maxim <mvlasov@phoenixit.ru>
2018-12-27 12:31:13 +0300

    Минимальный набор клиента на react

--- AlphaParking/AlphaParking.Web.Client/clientApp/components/Home.tsx
+++ AlphaParking/AlphaParking.Web.Client/clientApp/components/Home.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import '../stylesheets/common.scss';
 
 export default class Home extends React.Component {
     render() {
         return <div>
             <h1>Home page</h1>
             <Link to='/login' >Страница входа</Link>
+            <br />
             <Link to='/registration'>Страница регистрации</Link>
         </div>;
     }
